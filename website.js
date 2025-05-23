ca00ac6932cd51422bd2e088cf8193aca0415c6c
felicitywright <felicitywright2410@gmail.com>
2025-05-23 18:48:16 +1200

    Adding a forgotten line of code

---
+++ website.js
@@ -0,0 +1,10 @@
+JavaScript:
+function openMenu() {
+    menuBar.style.display = 'initial';
+}
+function reset() {
+    menuBox.style.display = 'initial';
+    menuBar.style.display = 'none';
+}
+menuBox.onmouseover = openMenu;
+menuBar.onmouseleave = reset;
